d03ba3ed05e548e60d5bb9c2faaec1f2212930e1
pthwong <tszhim.wong1010@yahoo.com>
2023-06-10 22:18:32 +0800

    Functions for removing the cat details created

--- src/components/CatDetails.js
+++ src/components/CatDetails.js
@@ -1,6 +1,6 @@
 import { async } from "q";
 import { useState, useEffect } from "react";
-import { useParams, useLocation, Link } from "react-router-dom";
+import { useParams, useLocation, useNavigate, Link } from "react-router-dom";
 import { ThemeProvider, Container, Box, Typography, CircularProgress, Button, Grid } from '@mui/material';
 import { createTheme } from "@mui/material/styles";
 
@@ -8,6 +8,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 function CatDetails() {
+  const navigate = useNavigate();
   const { catID } = useParams();
 
   const [catDetails, setCatDetails] = useState({});
@@ -20,8 +21,6 @@ function CatDetails() {
     setUserRole(localStorage.getItem('role'));
 
   }, [userEmail, userRole]);
-
-
 
   useEffect(() => {
     const fetchCatData = async () => {
@@ -36,6 +35,31 @@ function CatDetails() {
     fetchCatData();
   }, [catID]);
 
+  const handleDelCat = async (catID) => {
+    const confirmation = window.confirm('Are you sure to delete the details of the cat?');
+  
+    if (confirmation) {
+      try {
+        const response = await fetch(`/v1/delCatDetails/${catID}`, {
+          method: 'DELETE'
+        });
+  
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+  
+        const data = await response.json();
+        console.log('Cat successfully deleted:', data);
+
+        alert('The details of the cat are successfully deleted');
+        navigate('/');
+  
+      } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        alert('Error occured');
+      }
+    }
+  };
   const theme = createTheme({
     palette: {
       primary: {
@@ -114,7 +138,11 @@ function CatDetails() {
                     </Button> 
 
                     <Button variant="contained" color="primary" sx={{ mt: 2 }} component={Link} to={`/updateCatDetails/${catID}`} >
-                      Edit information
+                      Update cat details
+                    </Button> 
+
+                    <Button variant="contained" color="primary" sx={{ mt: 2 }} onClick={() => handleDelCat(catID)} >
+                      Remove the details of the cat
                     </Button> 
                   </>
                   : null
